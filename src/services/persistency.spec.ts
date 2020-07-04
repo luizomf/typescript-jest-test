4137aab8a9ebb3428c77c2caa4ba1124e556fe12
Luiz Otavio <luizomf@gmail.com>
2020-07-04 06:14:42 -0300

    configurando o jest

---
+++ src/services/persistency.spec.ts
@@ -0,0 +1,13 @@
+describe('TESTANDO ALGUMA COISA', () => {
+  it('should return one', () => {
+    const number = 1;
+    expect(number).toBe(1);
+  });
+});
+
+describe('TESTANDO OUTRA COISA', () => {
+  test('should return Luiz', () => {
+    const nome = 'Luiz';
+    expect(nome).toBe('Luiz');
+  });
+});
