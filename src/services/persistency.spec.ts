236485a04d6962130ec61bd6bd1d5932900658cc
Luiz Otavio <luizomf@gmail.com>
2020-07-05 18:07:55 -0300

    jest spyon

--- src/services/persistency.spec.ts
+++ src/services/persistency.spec.ts
@@ -1,13 +1,25 @@
-describe('TESTANDO ALGUMA COISA', () => {
-  it('should return one', () => {
-    const number = 1;
-    expect(number).toBe(1);
-  });
-});
+import { Persistency } from './persistency';
 
-describe('TESTANDO OUTRA COISA', () => {
-  test('should return Luiz', () => {
-    const nome = 'Luiz';
-    expect(nome).toBe('Luiz');
+describe('Persistency', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return undefined', () => {
+    // System under test
+    const sut = new Persistency();
+    expect(sut.saveOrder()).toBeUndefined();
+  });
+
+  it('should call console.log once', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call console.log with "Pedido salvo com sucesso..."', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledWith('Pedido salvo com sucesso...');
   });
 });
