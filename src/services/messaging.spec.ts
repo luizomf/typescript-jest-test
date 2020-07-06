6fed4192382134c66325646c2ad5e605bd882292
Luiz Otavio <luizomf@gmail.com>
2020-07-06 00:28:42 -0300

    Testando messaging

---
+++ src/services/messaging.spec.ts
@@ -0,0 +1,29 @@
+import { Messaging } from './messaging';
+
+const createSut = () => {
+  return new Messaging();
+};
+
+describe('Messaging', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return undefined', () => {
+    // System under test
+    const sut = createSut();
+    expect(sut.sendMessage('teste')).toBeUndefined();
+  });
+
+  it('should call console.log with "Mensagem enviada:" and msg', () => {
+    const sut = createSut();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.sendMessage('teste');
+    expect(consoleSpy).toHaveBeenCalledWith('Mensagem enviada:', 'teste');
+  });
+
+  it('should call console.log once', () => {
+    const sut = createSut();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.sendMessage('teste');
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+});
